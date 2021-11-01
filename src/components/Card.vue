ea9389aae068a608223738ff82f9415b593a9b5e
Milan Matull <milan@factorial.io>
2021-11-01 20:18:28 +0100

    initial commit

---
+++ src/components/Card.vue
@@ -0,0 +1,33 @@
+<template>
+  <p class="Card">
+    <a v-if="url" :href="url">{{ title }}</a>
+    <template v-else>{{ title }}</template>
+    <br />
+    <small
+      >{{ description }}<br />
+      <span class="Text-location">{{ location }}</span>
+    </small>
+  </p>
+</template>
+<script>
+export default {
+  props: {
+    url: {
+      type: String,
+      required: false,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
