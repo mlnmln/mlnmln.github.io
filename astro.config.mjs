13aeb391731cd1a7b23bbc79755c7a7996b7637c
Milan Matull <milan@factorial.io>
2023-07-28 17:43:08 +0200

    chore: install tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'http://www.milanmatull.com'
+  site: 'http://www.milanmatull.com',
+  integrations: [tailwind()]
 });
